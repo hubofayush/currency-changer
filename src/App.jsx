d37f96c3eb8c5733054d6abea88fbf751edaf018
Ayush Amberkar <ayushempire03@gmail.com>
2024-07-08 21:06:55 +0530

    added custom hook

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-orange-500 text-3xl text-center">Currency Exchange</h1>
+    </>
+  );
+}
+
+export default App;
